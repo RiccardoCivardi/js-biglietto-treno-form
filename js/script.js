59ab15e7455d4f7fe70673b4e59826a10bd6430d
RiccardoCivardi <riccardo.civardi@icloud.com>
2022-10-12 16:05:48 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/* 
+UTENTE --> Nome - Cognome
+UTENTE --> Km da percorrere
+UTENTE --> Selezionare la fascia d'età (maggiorenne, minorenne, over 65)
+Prezzo biglietto = km * 0.21€/km
+Sconto minorenni: 20%
+Sconto over 65: 40% 
+---> Stampare in pagina biglietto con:
+Nome e Cognome, Costo del biglietto (2 decimali) 
+*/
+// ----------------------------------
+
+
+/* DATA LAYER */
+
+
+
+// ----------------------------------
+
+
+/* BUSINESS LAYER */
+
+
+
+// ----------------------------------
+
+
+/* PRESENTATION LAYER */
