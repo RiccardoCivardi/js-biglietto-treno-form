b45cea2172acad06dc5787ecbec179a8ec8135f0
RiccardoCivardi <riccardo.civardi@icloud.com>
2022-10-12 23:39:33 +0200

    [add] js

--- js/script.js
+++ js/script.js
@@ -13,16 +13,99 @@ Nome e Cognome, Costo del biglietto (2 decimali)
 
 /* DATA LAYER */
 
+// default
+const priceForKm = 0.21;
+const discountJunior = 0.2;
+const discountSenior = 0.4;
+const btnReset = document.querySelector('#btn-clear');
+const info = document.querySelector('#info');
+let isInfoVisible = false;
+const btnInput = document.querySelector('#btn-input');
 
+// inseriti dall'utente
+let namePassenger = document.querySelector('#nome-cognome');
+let kmPassenger = document.querySelector('#km-tratta');
+let agePassenger = document.querySelector('#fascia-eta');
 
-// ----------------------------------
+// inizializzate
+let totalPrice, typeTicket;
+
+//output 
+/* PRESENTATION LAYER */
+
+const priceTicket = document.querySelector('#price-ticket');
+const nameTicket = document.querySelector('#name-passenger');
+const Ticket = document.querySelector('#type-ticket');
 
 
 /* BUSINESS LAYER */
 
+// info che appare al click
+
+info.addEventListener('click',toggleText);
+
+function toggleText(){
+  
+  if(isInfoVisible){
+    document.querySelector('ul').classList.add('d-none');
+    isInfoVisible = false;
+  }else{
+    document.querySelector('ul').classList.remove('d-none');
+    isInfoVisible = true;
+  }
+}
+
+// logica del biglietto
+
+btnInput.addEventListener('click',ticket);
+
+function ticket(){
+  //prezzo standard
+  totalPrice = kmPassenger.value * priceForKm;
+  typeTicket = "standard";
+  
+  //applico lo sconto se necessario e assegno il tipo di biglietto
+  if (agePassenger.value === "minorenne") {
+    totalPrice *= 1 - discountJunior;
+    typeTicket = "junior";
+  } else if (agePassenger.value === "senior") { 
+    totalPrice *= 1 - discountSenior;
+    typeTicket = "senior";
+  }
+  
+  /* PRESENTATION LAYER  */
+  priceTicket.innerHTML = totalPrice.toFixed(2);
+  nameTicket.innerHTML = namePassenger.value;
+  Ticket.innerHTML = typeTicket;
+
+};
 
 
-// ----------------------------------
+// reset con il bottone annulla sia degli input che del biglietto
+
+btnReset.addEventListener('click', function(){
+  kmPassenger.value = '';
+  namePassenger.value = '';
+  agePassenger.value = 'maggiorenne';
+
+  /* PRESENTATION LAYER  */
+  priceTicket.innerHTML = '';
+  nameTicket.innerHTML = '';
+  Ticket.innerHTML = '';
+})
 
 
-/* PRESENTATION LAYER */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
